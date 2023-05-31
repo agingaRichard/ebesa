4062bb3144cd2e3bdbe9b60f6d1034d6d684e2d0
agingaRichard <richardaginga@gmail.com>
2023-06-01 02:46:55 +0300

    Switched the db connection from Pocketbase (offline) to Pockethost.io (online).

--- pages/api/pocketbase.js
+++ pages/api/pocketbase.js
@@ -1,6 +1,7 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("http://127.0.0.1:8090");
+//const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase("https://sweet-optician.pockethost.io");
 
 //disabling autocancellation
 pb.autoCancellation(false);
